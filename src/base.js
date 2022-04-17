fe754b10ca61831acbbcee27be6be104bd40b52c
LeDuyDuong <ITITUN20004@student.hcmiu.edu.vn>
2022-04-17 21:24:38 +0700

    checklan1

---
+++ src/base.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './css/base.css';
+
+function base(){
+    return <div>
+        <div>
+            <div class="base">
+                <div class="title">
+                    <ul>
+                        <li>@Pikabook</li>
+                        <li>Products</li>
+                        <li>Developers</li>
+                        <li>Pricing</li>
+                        <li>Blog</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default base();
